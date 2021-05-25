18567d746aa02a4b4bba999d11f78a67ffc67e0a
verafes@gmail.com <verafes@gmail.com>
2021-05-25 16:25:46 -0700

    8 kyu - Multiply
    Created

---
+++ JavaScript/8kyu.Multiply.js
@@ -0,0 +1,19 @@
+// Solutions in JavaScript
+
+// 8kyu - Multiply
+
+https://www.codewars.com/kata/50654ddff44f800200000004/javascript
+
+// This code does not execute properly. Try to figure out why.
+
+// Given:
+
+function multiply(a, b){
+  a * b
+}
+
+// Solutions
+
+function multiply(a, b){
+  return a * b
+}

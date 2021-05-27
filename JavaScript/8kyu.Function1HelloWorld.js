a28c62f65630a3e477fccea6db1efdaa5eb5fcd2
verafes@gmail.com <verafes@gmail.com>
2021-05-26 23:38:52 -0700

    8kyu - Returning Strings
    Created

--- JavaScript/8kyu.Function1HelloWorld.js
+++ JavaScript/8kyu.Function1HelloWorld.js
@@ -4,7 +4,7 @@
 
 https://www.codewars.com/kata/function-1-hello-world
 
-*/ Description:
+/* Description:
 Make a simple function called greet that returns the most-famous "hello world!".
 
 Style Points

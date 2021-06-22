f58fa23624426544c126d6447beb6069b1736a4a
verafes <verafes@gmail.com>
2021-06-21 22:09:36 -0700

    8kyu - Function 3 - Multiplying two numbers
    Created

---
+++ Python/8kyu.Function3-MultiplyingTwoNumbers.py
@@ -0,0 +1,17 @@
+# Solutions in Python
+
+# 8kyu - Function 3 - Multiplying two numbers
+
+https://www.codewars.com/kata/50654ddff44f800200000004
+
+# This code does not execute properly. Try to figure out why.
+
+# Given
+
+def multiply(a, b):
+  a * b
+
+# Solution
+
+def multiply(a, b):
+  return a * b

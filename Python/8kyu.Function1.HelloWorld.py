0f79e88a506b23cb7ac97b1f05d416ad47febab3
verafes <verafes@gmail.com>
2021-06-22 22:48:49 -0700

    8kyu - Function 1 - hello world
    created

---
+++ Python/8kyu.Function1.HelloWorld.py
@@ -0,0 +1,14 @@
+# Solutions in Python
+
+# 8kyu - Function 1 - hello world
+
+https://www.codewars.com/kata/function-1-hello-world
+
+# Complete the function body for this hello() function so that it takes one argument (person, a string) 
+# and returns a string saying hello to that person. The result should be formatted so that when you call the function like this:
+# hello('Grae')
+# you return a string that looks like this:
+# 'Hello, Grae'
+
+def hello(name):
+    return f"Hello, {name}"

bf0781b0fac17a356b3b0e7df08a1c45b50d12d1
verafes <verafes@gmail.com>
2021-06-22 22:57:26 -0700

    8kyu - Opposite number
    created

---
+++ Python/8kyu - Opposite number.py
@@ -0,0 +1,11 @@
+# Solutions in Python
+
+# 8kyu - Opposite number
+
+https://www.codewars.com/kata/opposite-number
+
+# Very simple, given a number, find its opposite.
+
+def opposite(number):
+      return -number 
+name = a + b

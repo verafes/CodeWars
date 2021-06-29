313202b39030f931c62c5258c53d4ad5e68eced2
verafes@gmail.com <verafes@gmail.com>
2021-06-28 21:10:58 -0700

    7kyu - Is Undefined?
    Created

---
+++ JavaScript/7kyu.IsUndefined.js
@@ -0,0 +1,16 @@
+// Solutions in JavaScript
+
+// 7kyu - Is Undefined?
+
+https://www.codewars.com/kata/564a6486c2070faee6000011
+
+/* Help Timmy remove undefined values from his array, 
+you are modifing a callback function which he uses on each element on his array.
+
+Task
+Your task is to finish the isUndefined method which returns true if the value is undefined :)
+*/ 
+
+function isUndefined(value) {
+  return value === undefined;
+}

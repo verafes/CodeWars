d434fba995ed2673cfaab148008047b891b857a0
verafes@gmail.com <verafes@gmail.com>
2021-06-28 21:08:10 -0700

    8kyu - Who ate the cookie?
    Created

---
+++ JavaScript/8kyu.WhoAteTheCookie.js
@@ -0,0 +1,28 @@
+// Solutions in JavaScript
+
+// 8kyu - Who ate the cookie?
+
+https://www.codewars.com/kata/55a996e0e8520afab9000055
+
+/* For this problem you must create a program that says who ate the last cookie. I
+f the input is a string then "Zach" ate the cookie. If the input is a float or an int then "Monica" ate the cookie. 
+If the input is anything else "the dog" ate the cookie. The way to return the statement is: 
+"Who ate the last cookie? It was (name)!"
+
+Ex: Input = "hi" --> Output = "Who ate the last cookie? It was Zach! 
+(The reason you return Zach is because the input is a string)
+
+Note: Make sure you return the correct message with correct spaces and punctuation.
+
+Please leave feedback for this kata. Cheers!
+*/ 
+
+function cookie(x){
+  if (typeof(x) === 'string') {
+        return "Who ate the last cookie? It was Zach!"
+  }  if (typeof(x) === 'boolean') {
+        return "Who ate the last cookie? It was the dog!"
+  }  else {
+        return "Who ate the last cookie? It was Monica!"
+  }
+}

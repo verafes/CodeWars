c1de1695010fc460bdf06e7cc94f99837ae4d9ab
verafes@gmail.com <verafes@gmail.com>
2021-07-02 23:25:25 -0700

    8kyu - Convert a Boolean to a String
    Created

---
+++ JavaScript/8kyu.ConvertABooleanToAString.js
@@ -0,0 +1,15 @@
+// Solutions in JavaScript
+
+// 8kyu - Convert a Boolean to a String
+
+https://www.codewars.com/kata/convert-a-boolean-to-a-string
+
+/* Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given.
+*/ 
+
+function booleanToString(b){
+  return String(b);
+}
+

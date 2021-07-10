4cd9fd7a625a63a0bccbb5665be822bbc2cabe48
verafes@gmail.com <verafes@gmail.com>
2021-07-09 20:03:54 -0700

    7kyu - Return the closest number multiple of 10
    Created

---
+++ JavaScript/7kyu.ReturnTheClosestNumberMultipleOf10.js
@@ -0,0 +1,21 @@
+// Solutions in JavaScript
+
+// 7kyu - Return the closest number multiple of 10
+
+https://www.codewars.com/kata/return-the-closest-number-multiple-of-10
+
+/* Given a number return the closest number to it that is divisible by 10.
+
+Example input:	
+22
+25
+37
+Expected output:
+20
+30
+40
+*/ 
+
+const closestMultiple10 = num => {
+  return Math.round(num / 10) * 10;
+};

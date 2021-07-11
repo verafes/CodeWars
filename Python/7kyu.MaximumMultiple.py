4a40b6810dd3d287fc90f4294d11f224ab28a8fc
verafes <verafes@gmail.com>
2021-07-10 22:40:15 -0700

    7kyu - Maximum Multiple
    Created

---
+++ Python/7kyu.MaximumMultiple.py
@@ -0,0 +1,15 @@
+# Solutions in Python
+
+# 7kyu - Maximum Multiple
+
+https://www.codewars.com/kata/5aba780a6a176b029800041c 
+
+# A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them.
+
+# Given:
+def main [verb, noun]
+return verb + noun
+
+# Solution:
+def main (verb, noun):
+    return verb + noun

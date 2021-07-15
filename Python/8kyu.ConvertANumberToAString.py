2c7c1a8bb54f097a10612caab0119f7319facc91
verafes <verafes@gmail.com>
2021-07-14 20:45:37 -0700

    8kyu - Convert a Number to a String!
    Created

---
+++ Python/8kyu.ConvertANumberToAString.py
@@ -0,0 +1,15 @@
+# Solutions in Python
+
+# 8kyu - Convert a Number to a String!
+
+https://www.codewars.com/kata/557b5e0bddf29d861400005d
+
+# We need a function that can transform a number into a string.
+# What ways of achieving this do you know?
+# Examples:
+# number_to_string(123) /* returns '123' */
+# number_to_string(999) /* returns '999' */
+
+def number_to_string(num):
+    # Return a string of the number here!
+    return str(num)

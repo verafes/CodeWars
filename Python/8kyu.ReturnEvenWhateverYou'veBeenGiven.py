0c322a5ab19242d90e281384df2fbeb5989fa9f8
verafes <verafes@gmail.com>
2021-07-19 17:44:11 -0700

    8kyu - Return Even Whatever You've Been Given
    Created

---
+++ Python/8kyu.ReturnEvenWhateverYou'veBeenGiven.py
@@ -0,0 +1,14 @@
+# Solutions in Python
+
+# 8kyu - Return Even Whatever You've Been Given
+
+https://www.codewars.com/kata/5f8828d56dbd530014c1e241
+
+# # Given integer n return even numbers as they are, but subtract 1 from odd numbers.
+# Your solution should be 32 or less characters long.
+# Examples
+# Input = 3, Output = 2
+# Input = 16, Output = 16
+# Input = 45, Output = 44
+
+def always_even(n): return n-n%2

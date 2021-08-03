11717a0e6767a21ff5073e9f10296fe55a862e9d
verafes <verafes@gmail.com>
2021-08-02 22:55:16 -0700

    7kyu - Initialize my name
    Created

---
+++ Python/7kyu.InitializeMyName.py
@@ -0,0 +1,20 @@
+# Solutions in Python
+
+# 7kyu - Initialize my name
+
+https://www.codewars.com/kata/5768a693a3205e1cc100071f
+
+# Some people just have a first napaypalme; some people have first and last names 
+# and some people have first, middle and last names.
+#You task is to initialize the middle names (if there is any).
+#Examples
+#'Jack Ryan'                   => 'Jack Ryan'
+#'Lois Mary Lane'              => 'Lois M. Lane'
+#'Dimitri'                     => 'Dimitri'
+#'Alice Betty Catherine Davis' => 'Alice B. C. Davis'
+
+def initialize_names(name):
+    l = name.split()
+    for i in range(1, len(l)-1):
+        l[i] = l[i][0] + '.'
+    return ' '.join(l)

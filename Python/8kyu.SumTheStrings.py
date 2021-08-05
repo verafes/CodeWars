2259a2fbb8f8b158aa674d9c77b67a732bc07186
verafes <verafes@gmail.com>
2021-08-04 23:06:15 -0700

    8kyu - Sum The Strings
    updated

--- Python/8kyu.SumTheStrings.py
+++ Python/8kyu.SumTheStrings.py
@@ -5,7 +5,7 @@
 https://www.codewars.com/kata/5966e33c4e686b508700002d
 
 # Create a function that takes 2 positive integers 
-in form of a string as an input, and outputs the sum (also as a string):
+# in form of a string as an input, and outputs the sum (also as a string):
 #  sumStr("4", "5")    // should output "9"
 #  sumStr("34", "5")   // should output "39"
 # If either input is an empty string, consider it as zero.

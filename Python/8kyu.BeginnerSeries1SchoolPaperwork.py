0d2ff40c438f508905788bdcf6261c4a69b0e0c4
verafes <verafes@gmail.com>
2021-08-10 22:57:46 -0700

    8kyu - Beginner Series #1 School Paperwork
    Created

---
+++ Python/8kyu.BeginnerSeries1SchoolPaperwork.py
@@ -0,0 +1,15 @@
+# Solutions in Python
+
+# 8kyu - Beginner Series #1 School Paperwork
+
+https://www.codewars.com/kata/55f9b48403f6b87a7c0000bd
+
+# Your classmates asked you to copy some paperwork for them. 
+# You know that there are 'n' classmates and the paperwork has 'm' pages.
+# Your task is to calculate how many blank pages do you need.
+# Example: paperwork(5, 5) == 25, Note: if n < 0 or m < 0 return 0!
+
+def paperwork(n, m):
+    if n < 0 or m < 0: 
+        return 0
+    return n * m

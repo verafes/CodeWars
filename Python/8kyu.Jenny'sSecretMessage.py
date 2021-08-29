331a0f5d0e0f3e552641b87f246c33ee24a9797f
verafes <verafes@gmail.com>
2021-08-28 21:42:25 -0700

    8kyu - Jenny's secret message
    Created

---
+++ Python/8kyu.Jenny'sSecretMessage.py
@@ -0,0 +1,23 @@
+# Solutions in Python
+
+# 8kyu - Jenny's secret message
+
+https://www.codewars.com/kata/55225023e1be1ec8bc000390 
+
+# Jenny has written a function that returns a greeting for a user. 
+# However, she's in love with Johnny, and would like to greet him slightly different. 
+# She added a special case to her function, but she made a mistake.
+# Can you help her?
+
+# Given
+
+function greet(name){
+  return "Hello, " + name + "!";
+  if(name === "Johnny")
+    return "Hello, my love!";
+}
+
+# Solution
+
+def greet(name):
+    return "Hello, my love!" if name == "Johnny" else f"Hello, {name}!"

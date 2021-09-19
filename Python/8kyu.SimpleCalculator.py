b8bff11d7e6b648447d2e1fe5b8fc9a30154b935
verafes <verafes@gmail.com>
2021-09-18 21:16:54 -0700

    8kyu - Simple calculator
    Update

--- Python/8kyu.SimpleCalculator.py
+++ Python/8kyu.SimpleCalculator.py
@@ -26,3 +26,10 @@ def calculator(x,y,op):
         else: 
             return "unknown value"
     return "unknown value"
+
+# Solution 2
+
+def calculator(x,y,op):
+    if str(op) not in '+-/*' or not str(x).isnumeric() or not str(y).isnumeric():
+        return 'unknown value'
+    return x + y if op == '+' else x - y if op == '-' else x * y if op == '*' else x / y

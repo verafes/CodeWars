6de6475342f8561544a8dff510c6155054e99e42
verafes <verafes@gmail.com>
2021-10-17 22:17:03 -0700

    8kyu - Convert boolean values to strings 'Yes' or 'No'
    Created

---
+++ Python/8kyu.ConvertBooleanValuesToStringsYesOrNo.py
@@ -0,0 +1,10 @@
+# Solutions in Python
+
+# 8kyu - Convert boolean values to strings 'Yes' or 'No'
+
+https://www.codewars.com/kata/53369039d7ab3ac506000467
+
+# Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+def bool_to_word(boolean):
+    return "Yes" if boolean == True else "No"

fc3f345db094cd233fa2555b6c9b649908439d81
verafes@gmail.com <verafes@gmail.com>
2021-11-03 22:53:30 -0700

    7kyu - Powers of 3
    Created

---
+++ JavaScript/7kyu.PowersOf3.js
@@ -0,0 +1,20 @@
+// Solutions in JavaScript
+
+// 7kyu - Powers of 3
+
+https://www.codewars.com/kata/powers-of-3
+
+/* Given a positive integer N, return the largest integer k such that 3^k < N.
+
+For example,
+
+largest_power(3) == 0
+largest_power(4) == 1
+You may assume that the input to your function is always a positive integer.
+*/ 
+
+function largestPower(n){
+  let k = 0;
+  while (3**k < n) k++;
+  return k-1;
+}

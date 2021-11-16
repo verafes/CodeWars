61084b0773fc7fcda63c108af014ea9a16f56c99
verafes <verafes@gmail.com>
2021-11-15 21:48:09 -0800

    8kyu - Convert a Boolean to a String
    Created

---
+++ Python/8kyu.ConvertBooleanToString.py
@@ -0,0 +1,10 @@
+# Solutions in Python
+
+# 8kyu - Convert a Boolean to a String
+
+https://www.codewars.com/kata/551b4501ac0447318f0009cd 
+
+# Implement a function which convert the given boolean value into its string representation.
+
+def boolean_to_string(b):
+    return str(b)

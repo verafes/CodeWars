e4635a6a72f92ed0c30acb536123331398aa2127
verafes <verafes@gmail.com>
2021-12-04 19:33:53 -0800

    8kyu - Hello, Name or World!
    Created

---
+++ Python/8kyu.HelloNameOrWorld.py
@@ -0,0 +1,21 @@
+# Solutions in Python
+
+# 8kyu - Hello, Name or World!
+
+https://www.codewars.com/kata/57e3f79c9cb119374600046b 
+
+# Define a method hello that returns "Hello, Name!" to a given name, or says Hello, World! 
+# if name is not given (or passed as an empty String).
+# Assuming that name is a String and it checks for user typos to return a name 
+# with a first capital letter (Xxxx).
+# Examples:
+# hello "john"   => "Hello, John!"
+# hello "aliCE"  => "Hello, Alice!"
+# hello          => "Hello, World!" # name not given
+# hello ''       => "Hello, World!" # name is an empty String
+
+def hello(name=""):
+    if name:
+        return f"Hello, {name.title()}!"
+    else:
+         return f"Hello, World!"

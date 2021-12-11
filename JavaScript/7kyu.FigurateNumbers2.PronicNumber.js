fd3ad1a4830d46ce974d4eb8415b14eaea3ede4d
verafes@gmail.com <verafes@gmail.com>
2021-12-10 23:06:23 -0800

    7kyu - Figurate Numbers #2 - Pronic Number
    Update

--- JavaScript/7kyu.FigurateNumbers2.PronicNumber.js
+++ JavaScript/7kyu.FigurateNumbers2.PronicNumber.js
@@ -25,12 +25,12 @@ Explanation:
 Note:
 Negative numbers are tested as well.
 */ 
-
+ 
 function isPronic(n){
   for (let i=0; i<=n; i++) {
     if (i*(i+1) === n){
       return true
       }
   }
-    return false
+  return false
 }

89790a98e23f677817c40acae0ce3fcfb7f7b8ac
verafes <verafes@gmail.com>
2021-12-20 21:47:54 -0800

    7kyu - How far will I go?
    Created

--- Python/7kyu.HowFarWillIGo.py
+++ Python/7kyu.HowFarWillIGo.py
@@ -6,7 +6,7 @@ https://www.codewars.com/kata/56d46b8fda159582e100001b
 
 # You have recently discovered that horses travel in a unique pattern - they're either running (at top speed) or resting (standing still).
 # Here's an example of how one particular horse might travel:
-# The horse Blaze can run at 14 metres/second for 60 seconds, but must then rest for 45 seconds.
+# The horse Blaze can run at 14 metres/second for 60 seconds, but must then rest for 45 seconds. 
 # After 500 seconds Blaze will have traveled 4200 metres.
 # Your job is to write a function that returns how long a horse will have traveled after a given time.
 
